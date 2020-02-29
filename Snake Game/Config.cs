6505aecda7d83b041f264bd945f263790ddb659d
Prashant Varshney <pv03158@gmail.com>
2020-02-29 22:21:01 +0530

    created a configuration file

---
+++ Snake Game/Config.cs
@@ -0,0 +1,16 @@
+﻿// this is the configuration file of Snake Game
+// alter this to change Screen Dimensions, box dimensions, etc.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Snake_Game
+{
+    class Config
+    {
+        // defining display configurations, this will alter screen dimensions of the game
+        public static uint SCREEN_HEIGHT = 960;
+        public static uint SCREEN_WIDTH = 1280;
+    }
+}
