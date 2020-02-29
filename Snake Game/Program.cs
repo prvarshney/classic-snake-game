a2a72edf85214ccb1b8518817fa4dcf53df4ad0a
Prashant Varshney <pv03158@gmail.com>
2020-02-29 21:40:10 +0530

    basic template

---
+++ Snake Game/Program.cs
@@ -0,0 +1,29 @@
+﻿// This game application built using SFML (simple and fast multimedia library).
+// This library handles graphics drawing, event listening, music and networking related operations
+
+using System;
+using SFML;
+using SFML.Graphics;
+using SFML.Audio;
+using SFML.System;
+using SFML.Window;
+
+namespace Snake_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // defining display configurations, this will alter screen dimensions of the game
+            uint SCREEN_HEIGHT = 960;
+            uint SCREEN_WIDTH = 1280;
+            RenderWindow window = new RenderWindow(new VideoMode(SCREEN_WIDTH,SCREEN_HEIGHT),"Snake Game");
+            // starting game loop, the game window is open till any closing event doesn't occurs in event stack
+            while (window.IsOpen)
+            {
+                window.Clear(Color.Blue);
+                window.Display();
+            }
+        }
+    }
+}
