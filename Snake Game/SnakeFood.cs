6bf4ab597ba1a21a0636a3b50eff3ac2ed3421d8
Prashant Varshney <pv03158@gmail.com>
2020-02-29 22:21:29 +0530

    added SnakeFood file

---
+++ Snake Game/SnakeFood.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using SFML;
+using SFML.Window;
+using SFML.Graphics;
+
+namespace Snake_Game
+{
+    class SnakeFood
+    {
+        
+    }
+}
